1ef8de03954319230906069d644ab0c7cce7d8c4
Md. Mustakim <mahfujapple95@gmail.com>
2025-02-14 20:23:47 +0600

    Number Swap

--- mod_22_Math_func.js
+++ mod_22_Math_func.js
@@ -1,15 +1,19 @@
 const minimum= Math.min(45,12,89,4,5,56);
 console.log(minimum);
 
+
 const maximum=Math.max(45,78,56,98,12,46,56);
 console.log(maximum);
+
 
 console.log(Math.PI)
 console.log(Math.abs(10-15));
 console.log(Math.round(8.99));
 
+
 console.log(Math.floor(8.99)); //return the floor value 8 instead of returning 9 like round .
 console.log(Math.ceil(8.05));//return the ceilling value 9 instead of returning 9 like floor .
+
 
 console.log(Math.random()*10);//This returns number from 1to 10 but with fraction.
 const rand=Math.round(Math.random()*10);
---
+++ mod_22_swap.js
@@ -0,0 +1,11 @@
+let a=5;
+let b=8;
+let temp;
+
+console.log("Numbers before swap: ",a,b);
+
+temp=a;
+a=b;
+b=temp;
+
+console.log("Numbers after swap: ",a,b);
